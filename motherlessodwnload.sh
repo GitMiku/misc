9bc3c802a812e4360c264ee08d5273558c07f544
GitMiku <MikuMiku@lavabit.com>
2014-05-23 01:51:11 -0400

    Create motherlessodwnload.sh

---
+++ motherlessodwnload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1"]
+then
+    echo "Usage: $0 link-motherless-vid filename"
+else
+    URL=$(curl -s $1)
+    TIDYURL=$(echo -e "$URL" | grep '"file"' | grep motherlessmedia | tr -d ' ' | sed s/'"file":"'// | sed s/'",'//)
+    if [ -z "$2" ]
+    then
+        wget --referer=motherless.com --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $TID$
+    else
+        wget --referer=motherless.com --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $TID$
+    fi
+fi
+
