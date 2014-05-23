028691eeece40c7341f5f821b2057f9b46f9522b
GitMiku <MikuMiku@lavabit.com>
2014-05-23 01:57:31 -0400

    line got cut off

--- motherlessodwnload.sh
+++ motherlessodwnload.sh
@@ -7,9 +7,8 @@ else
     TIDYURL=$(echo -e "$URL" | grep '"file"' | grep motherlessmedia | tr -d ' ' | sed s/'"file":"'// | sed s/'",'//)
     if [ -z "$2" ]
     then
-        wget --referer=motherless.com --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $TID$
+        wget --referer=motherless.com --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $TIDYURL
     else
-        wget --referer=motherless.com --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $TID$
+        wget --referer=motherless.com --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $TIDYURL --output-document=$2
     fi
 fi
-
