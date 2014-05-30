493f76ddc3d302c19251404c999e834a5a42a801
GitMiku <MikuMiku@lavabit.com>
2014-05-30 10:41:51 -0400

    Update 4chandownload.sh

--- 4chandownload.sh
+++ 4chandownload.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
-if [ -z "$1" ] #If no command line arguments
-then
-    echo "This script downloads all files (including webm) from a 4chan thread"
-    echo "Usage: $0 URL"
-    echo "Optionally you can take a screenshot of the thread too with (requires cutycapt to be installed):"
-    echo "$0 -s URL"
-    exit 1
-else
-    if [ "$1" == "-s" ] #If the first command line argument is "-s"
-    then
-        url=$2 #Then we set URL to the second command line argument
+#!/bin/bash                                                                                                                                                                                     
+if [ -z "$1" ] #If no command line arguments                                                                                                                                                    
+then                                                                                                                                                                                            
+    echo "This script downloads all files (including webm) from a 4chan thread"                                                                                                                 
+    echo "Usage: $0 URL"                                                                                                                                                                        
+    echo "Optionally you can take a screenshot of the thread too with (requires cutycapt to be installed):"                                                                                     
+    echo "$0 -s URL"                                                                                                                                                                            
+    exit 1                                                                                                                                                                                      
+else                                                                                                                                                                                            
+    if [ "$1" == "-s" ] #If the first command line argument is "-s"                                                                                                                             
+    then                                                                                                                                                                                        
+        url=$2 #Then we set URL to the second command line argument                                                                                                                             
+        screenshot=$(echo $url | sed s/'http:\/\/'// | sed s/'boards.4chan.org'// | sed s/'\/'/-/g | cut -d'-' -f2-10).png                                                                      
+        cutycapt --url=$url --out=$screenshot #Screenshot the page                                                                                                                              
     else
         url=$1 #Otherwise we set it to the first command line argument
     fi
-    #The outfile here. It should look like board-thread-threadnumber-threadname.png
-    #Example g-thread-42195412-desktop-thread.png
-    screenshot=$(echo $url | sed s/'http:\/\/'// | sed s/'boards.4chan.org'// | sed s/'\/'/-/g | cut -d'-' -f2-10).png
     #thread holds the html document
     thread=$(curl -s --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36" $url)
     #in files we extract the file URLs from $thread
@@ -26,9 +25,4 @@ else
         shift #And move on to the next
     done
     wait
-
-    if [ "$url" == "$2" ]
-    then
-        cutycapt --url=$url --out=$screenshot 
-    fi
 fi
